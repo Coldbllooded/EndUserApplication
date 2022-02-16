7547c83c14dba0f52c1a1ccc39aba60ddd88a3a3
BuildTools <unconfigured@null.spigotmc.org>
2022-02-16 03:38:01 -0500

    Cleaned up

--- src/main/java/com/FerrisIOT/HTTP/HttpController.java
+++ src/main/java/com/FerrisIOT/HTTP/HttpController.java
@@ -1,34 +1,11 @@
 package com.FerrisIOT.HTTP;
 
-import com.FerrisIOT.GetVal;
-import com.FerrisIOT.HTTPS;
-import com.FerrisIOT.Select;
-import org.apache.http.HttpEntity;
-import org.apache.http.auth.AuthScope;
-import org.apache.http.auth.UsernamePasswordCredentials;
-import org.apache.http.client.CredentialsProvider;
-import org.apache.http.client.methods.CloseableHttpResponse;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.BasicCredentialsProvider;
-import org.apache.http.impl.client.CloseableHttpClient;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.client.HttpClients;
-import org.apache.http.util.EntityUtils;
-
-import javax.net.ssl.SSLContext;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.security.NoSuchAlgorithmException;
-import java.util.LinkedList;
-
 public class HttpController {
     public static String HTTPC(String UN, String PW){
         String Status = "Null";
         String URI = "https://10.35.80.77:8000/test";
         //Get
-        HTTPS.HTTPs(URI);
+        //HTTPS.HTTPs(URI);
         //HTTPS.HTTPGET(URI);
         //Post
         //CloseableHttpResponse response = HTTPS.HTTPPOST(URI,UN,PW);
@@ -47,7 +24,7 @@ public class HttpController {
         //String[] UID = Status.split("\\|");
         //Session ID = UID[0]
         //User ID = UID[1]
-        LinkedList names = GetVal.GetData(Status);
+        LinkedList names = Operations.GetData(Status);
         new Select(names);
 
 
