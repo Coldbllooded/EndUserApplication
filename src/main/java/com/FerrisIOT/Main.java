e48d591ca098e84ce7c5c7d0f91db796de43d853
TriageONE <triage.sector1@gmail.com>
2022-02-19 23:35:51 -0500

    Added empty object Authentication to be modified when authentication happens

--- src/main/java/com/FerrisIOT/Main.java
+++ src/main/java/com/FerrisIOT/Main.java
@@ -5,9 +5,11 @@ import com.sun.security.jgss.InquireSecContextPermission;
 import javax.swing.*;
 import java.net.URL;
 import java.util.LinkedList;
+import java.util.TimeZone;
 
 public class Main {
 
+    public static Authenticator authenticator = new Authenticator();
     public static void main(String[] args) {
 	// write your code here
         Login.CreateLogin();
